7ac094dfeddf5219fd4c9da947d5a6f54746ed18
Trevor Vaughan <tvaughan@onyxpoint.com>
2015-11-19 21:07:38 -0500

    Optimized code, pulled off results

--- spec/acceptance/catalog_diff_spec.rb
+++ spec/acceptance/catalog_diff_spec.rb
@@ -10,36 +10,42 @@ describe 'Catalog Diff Tool' do
     EOS
   }
 
-  hosts.each do |host|
-    context 'build present catalog' do
-      it 'should work' do
-        result = apply_manifest_on(
-          host,
-          manifest,
-          :catch_failures => true
-        )
+  _catalog_dir = File.join(ENVIRONMENT_SPEC[:project_root],'catalogs')
+  let(:catalog_dir){ _catalog_dir }
 
-        client_info = fact_on(host,'os')
-        tmp_manifest = result.cmd.split(/\s+/).last
-        output_catalog = %(#{fact_on(host,'fqdn')}-#{client_info['name']}-#{client_info['release']['full']}-present-catalog.json )
+  let(:run_id){ Time.now.to_f.to_s }
 
-        manifestdir = host.puppet['manifestdir']
-        on(host, %(mkdir -p #{manifestdir} && mv #{tmp_manifest} #{manifestdir}/site.pp))
+  unless File.directory?(_catalog_dir)
+    FileUtils.mkdir_p(_catalog_dir)
+  end
 
-        on(
-          host,
-          %(puppet master --compile #{fact_on(host,'fqdn')} > #{output_catalog})
-        )
-      end
+  def collect_catalog(parser_type)
+    client_info = fact_on(host,'os')
+    tmp_manifest = result.cmd.split(/\s+/).last
+    output_catalog = %(#{run_id}-#{fact_on(host,'fqdn')}-#{client_info['name']}-#{client_info['release']['full']}-#{catalog_type}-catalog.json )
+
+    manifestdir = host.puppet['manifestdir']
+    on(host, %(mkdir -p #{manifestdir} && mv #{tmp_manifest} #{manifestdir}/site.pp))
+
+    catalog = on(host,%(puppet master --compile --parser=#{parser_type} #{fact_on(host,'fqdn')})).stdout
+
+    File.open(File.join(catalog_dir,output_catalog),'w') do |fh|
+      fh.puts(catalog)
     end
-    context 'build future catalog' do
-      it 'should work' do
-        apply_manifest_on(
-          host,
-          manifest,
-          :catch_failures => true,
-          :future_parser => true
-        )
+  end
+
+  hosts.each do |host|
+    ['current','future'].each do |parser_type|
+      context "#{parser_type} parser" do
+        it 'should work' do
+          result = apply_manifest_on(
+            host,
+            manifest,
+            :catch_failures => true
+          )
+
+          collect_catalog(parser_type)
+        end
       end
     end
   end
